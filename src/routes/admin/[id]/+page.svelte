e04b09ac0c8eddd266e86ad7834854bb71296d89
Lukas Mörtman <lukas@mocko.se>
2022-12-23 16:26:02 +0100

    created update page

--- src/routes/admin/[id]/+page.svelte
+++ src/routes/admin/[id]/+page.svelte
@@ -6,44 +6,70 @@
 	import { goto } from '$app/navigation';
     let visaQr: boolean = false;
     let kvitto: any = {};
+    let bild: string;
+    let formData: FormData = new FormData();
+
 
    onMount(async () => {
 		kvitto = await pb.collection('kvitton').getOne(data.kvitto.id);
+        bild = `https://nfkvitton.fly.dev/api/files/kvitton/${kvitto.id}/${kvitto.bild}?thumb=120x150`
 	});
+
+    async function taBortKvitto() {
+        await pb.collection('kvitton').delete(data.kvitto.id);
+        goto('/admin');
+    }
+    async function updateraBild(bilden: (Event & { currentTarget: EventTarget & HTMLInputElement; })){
+        bild = (URL.createObjectURL(bilden.currentTarget.files![0]));
+        kvitto.bild = bilden.currentTarget.files![0];
+    }
+    async function updateraKvitto() {
+        formData.append("vara", kvitto.vara);
+        formData.append("pris", kvitto.pris.toString());
+        formData.append("datum", kvitto.datum.toString());
+        formData.append("bild", kvitto.bild);
+        formData.append("kategori", kvitto.kategori);
+        formData.append("swish", kvitto.swish);
+        formData.append("fixad", kvitto.fixad);
+        formData.append("typavkop", kvitto.typavkop); 
+        await pb.collection('kvitton').update(data.kvitto.id, formData);
+        goto('/admin');
+    }
+
 </script>
 
 {#if !visaQr}
     <div class="huvudStilen">
         <div class="knappStilen">
             <button on:click={() =>  {goto('/admin')}}>Tillbaka till admin</button>
-            <button on:click={() => console.log(kvitto.id)}>Ta bort kvitto</button>
+            <button on:click={() => taBortKvitto()}>Ta bort kvitto</button>
             <button on:click={() => visaQr = true}>Visa QR-kod</button>
         </div>
         <div class="divByt">
             <div>
                 <label class="pekare">
                     <img
-                    src={`https://nfkvitton.fly.dev/api/files/kvitton/${kvitto.id}/${kvitto.bild}?thumb=120x150`}
+                    src={bild}
                     alt={'bild på kvittot'}
                     width={120}
                     height={150}
                     loading={'lazy'}
                 />
-                <input type="file" name="Bild" class="hide" />
+                <input on:input={(e)=>updateraBild(e)} type="file" name="Bild" class="hide" />
                 </label>
             </div>
-            <form class="formStilen">
+            <form on:submit|preventDefault={updateraKvitto} class="formStilen">
                 <label>
                     Namn på köpet:
-                    <input  type="text" name="Vara" value={kvitto.vara} />
+                    <input  type="text" name="Vara" bind:value={kvitto.vara} />
                 </label>
                 <label>
                     Pris på köpet:
-                    <input type="number" name="Pris" value={kvitto.pris} />
+                    <input type="number" name="Pris" bind:value={kvitto.pris} />
                 </label>
                 <label>
                     Kategori på köpet:
-                    <select  name="Kategori" value={kvitto.kategori}>
+                    <select  name="Kategori" bind:value={kvitto.kategori}>
                         <option value="Laborationer" name="Laborationer">Laborationer</option>
                         <option value="Medlemsavgifter" name="Medlemsavgifter">Medlemsavgifter</option>
                         <option value="Kök&fester">Kök & fester</option>
@@ -54,19 +80,19 @@
                 </label>
                 <label>
                     Datum köpet skedde:
-                    <input  type="date" name="Datum" value={kvitto.datum} />
+                    <input  type="date" name="Datum" bind:value={kvitto.datum} />
                 </label>
                 <label>
                     Swish-nummer:
-                    <input  type="tel" name="Swish" value={kvitto.swish} />
+                    <input  type="tel" name="Swish" bind:value={kvitto.swish} />
                 </label>
                 <label>
                     Fixad:
-                    <input type="checkbox" name="Fixad" checked={kvitto.fixad} />
+                    <input type="checkbox" name="Fixad" bind:checked={kvitto.fixad} />
                 </label>
                 <label>
                     Typ av köp:
-                    <select  name="Typavköp" value={kvitto.typavkop}>
+                    <select  name="Typavköp" bind:value={kvitto.typavkop}>
                         <option value="Avgift" name="Avgift">Avgift</option>
                         <option value="Intäkt" name="Intäkt">Intäkt</option>
                     </select>
